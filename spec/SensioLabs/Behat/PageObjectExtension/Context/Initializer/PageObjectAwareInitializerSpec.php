c2d3caa5df4f5a842470cedc082bf81d4b334469
Jakub Zalas <jakub@zalas.pl>
2013-05-13 10:33:29 +0100

    Fixed interface mocks (willImplement => implement).

--- spec/SensioLabs/Behat/PageObjectExtension/Context/Initializer/PageObjectAwareInitializerSpec.php
+++ spec/SensioLabs/Behat/PageObjectExtension/Context/Initializer/PageObjectAwareInitializerSpec.php
@@ -19,23 +19,23 @@ class PageObjectAwareInitializerSpec extends ObjectBehavior
 
     function it_supports_page_object_aware($context)
     {
-        $context->willImplement('SensioLabs\Behat\PageObjectExtension\Context\PageObjectAwareInterface');
-        $context->willImplement('Behat\Behat\Context\ContextInterface');
+        $context->implement('SensioLabs\Behat\PageObjectExtension\Context\PageObjectAwareInterface');
+        $context->implement('Behat\Behat\Context\ContextInterface');
 
         $this->supports($context)->shouldReturn(true);
     }
 
     function it_should_not_support_other_contexts($context)
     {
-        $context->willImplement('Behat\Behat\Context\ContextInterface');
+        $context->implement('Behat\Behat\Context\ContextInterface');
 
         $this->supports($context)->shouldReturn(false);
     }
 
     function it_should_inject_the_page_factory_into_the_context($context, $pageFactory)
     {
-        $context->willImplement('SensioLabs\Behat\PageObjectExtension\Context\PageObjectAwareInterface');
-        $context->willImplement('Behat\Behat\Context\ContextInterface');
+        $context->implement('SensioLabs\Behat\PageObjectExtension\Context\PageObjectAwareInterface');
+        $context->implement('Behat\Behat\Context\ContextInterface');
 
         $context->setPageFactory($pageFactory)->shouldBeCalled();
 
